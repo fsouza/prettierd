2d9369bf5efc56a331d96fde111eba3ab3a64ee1
William Boman <william@redwill.se>
2021-04-17 05:28:27 +0200

    let prettier infer parser by providing filename (#162)
    * let prettier infer parser by providing filename
    
    * :Format 😊
    
    * remove withParser func

--- src/service.ts
+++ src/service.ts
@@ -3,45 +3,14 @@ process.env.FORCE_COLOR = "0";
 import LRU from "nanolru";
 import path from "path";
 import prettier from "prettier";
-import { BuiltInParserName, Options, format } from "prettier";
+import { Options, format } from "prettier";
 
 const configCache = new LRU({ max: 20, maxAge: 60000 });
 
-const getParserName = (ext: string): BuiltInParserName => {
-  switch (ext) {
-    case ".tsx":
-    case ".ts":
-      return "typescript";
-    case ".css":
-    case ".scss":
-      return "css";
-    case ".html":
-      return "html";
-    case ".json":
-      return "json";
-    case ".yml":
-    case ".yaml":
-      return "yaml";
-    case ".md":
-    case ".markdown":
-    case ".mkd":
-      return "markdown";
-    default:
-      return "babel";
-  }
-};
-
-function withParser(options: Options | null, filePath: string): Options {
-  return {
-    ...options,
-    parser: getParserName(path.extname(filePath)),
-  };
-}
-
-async function resolveConfig(cwd: string, filePath: string): Promise<Options> {
+async function resolveConfig(cwd: string, filepath: string): Promise<Options> {
   let v = configCache.get<string, Options>(cwd);
   if (!v) {
-    v = await prettier.resolveConfig(filePath, {
+    v = await prettier.resolveConfig(filepath, {
       editorconfig: true,
       useCache: false,
     });
@@ -49,7 +18,10 @@ async function resolveConfig(cwd: string, filePath: string): Promise<Options> {
       configCache.set(cwd, v);
     }
   }
-  return withParser(v, filePath);
+  return {
+    ...v,
+    filepath,
+  };
 }
 
 function resolveFile(cwd: string, fileName: string): [string, string] {
