af026946aad18375d3f8d268bbbeea662b094ca2
francisco souza <fsouza@users.noreply.github.com>
2021-05-10 17:13:11 -0400

    Simplify code

--- src/prettierd.ts
+++ src/prettierd.ts
@@ -1,28 +1,55 @@
-import fs from "fs";
+#!/usr/bin/env node
+
+import fs from "fs/promises";
 import path from "path";
 
-const core_dTitle = "prettierd";
-
-function getDotfile(): string {
+async function getDotfile(title: string): Promise<string> {
   if (process.env.CORE_D_DOTFILE) {
     return process.env.CORE_D_DOTFILE;
   }
 
   if (process.env.HOME && process.env.XDG_CONFIG_HOME) {
-    const confDir = path.join(process.env.XDG_CONFIG_HOME, core_dTitle);
-    fs.mkdirSync(confDir, { recursive: true });
+    const confDir = path.join(process.env.XDG_CONFIG_HOME, title);
+    await fs.mkdir(confDir, { recursive: true });
 
-    const confPath = path.join(confDir, core_dTitle);
+    const confPath = path.join(confDir, title);
     return path.relative(process.env.HOME, confPath);
   }
 
   return ".prettierd";
 }
 
-process.env.CORE_D_TITLE = core_dTitle;
-process.env.CORE_D_SERVICE = require.resolve("../dist/service");
-process.env.CORE_D_DOTFILE = getDotfile();
+async function main(cmd: string): Promise<void> {
+  const title = "prettierd";
 
-import { main } from "./main";
+  process.env.CORE_D_TITLE = title;
+  process.env.CORE_D_SERVICE = require.resolve("./service");
+  process.env.CORE_D_DOTFILE = await getDotfile(title);
 
-main(process.argv[2]);
+  const core_d = require("core_d");
+
+  if (
+    cmd === "start" ||
+    cmd === "stop" ||
+    cmd === "restart" ||
+    cmd === "status"
+  ) {
+    core_d[cmd]();
+    return;
+  }
+
+  const fileName = cmd;
+  let text = "";
+  process.stdin.setEncoding("utf-8");
+  process.stdin.on("data", (chunk) => {
+    text += chunk;
+  });
+  process.stdin.on("end", () => {
+    core_d.invoke([fileName], text);
+  });
+}
+
+main(process.argv[2]).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
