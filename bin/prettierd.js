0e05292cb5524392a9c0b80ced0a25185b9c5cc5
francisco souza <fsouza@users.noreply.github.com>
2021-05-10 16:35:24 -0400

    bin/prettierd: response CORE_D_DOTFILE if it's already set

--- bin/prettierd.js
+++ bin/prettierd.js
@@ -3,20 +3,28 @@
 const path = require("path");
 const fs = require("fs");
 
-process.env.CORE_D_TITLE = "prettierd";
-process.env.CORE_D_DOTFILE = ".prettierd";
-process.env.CORE_D_SERVICE = require.resolve("../dist/service");
+function getDotfile() {
+  if (process.env.CORE_D_DOTFILE) {
+    return process.env.CORE_D_DOTFILE;
+  }
 
-if (process.env.HOME && process.env.XDG_CONFIG_HOME) {
-  const confDir = path.join(
-    process.env.XDG_CONFIG_HOME,
-    process.env.CORE_D_TITLE
-  );
-  fs.mkdirSync(confDir, { recursive: true });
+  if (process.env.HOME && process.env.XDG_CONFIG_HOME) {
+    const confDir = path.join(
+      process.env.XDG_CONFIG_HOME,
+      process.env.CORE_D_TITLE
+    );
+    fs.mkdirSync(confDir, { recursive: true });
 
-  const confPath = path.join(confDir, process.env.CORE_D_TITLE);
-  process.env.CORE_D_DOTFILE = path.relative(process.env.HOME, confPath);
+    const confPath = path.join(confDir, process.env.CORE_D_TITLE);
+    return path.relative(process.env.HOME, confPath);
+  }
+
+  return ".prettierd";
 }
+
+process.env.CORE_D_TITLE = "prettierd";
+process.env.CORE_D_SERVICE = require.resolve("../dist/service");
+process.env.CORE_D_DOTFILE = getDotfile();
 
 const { main } = require("../dist/main");
 
