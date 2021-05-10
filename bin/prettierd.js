09b289959eb825db92507379f05bcad2a94beb2e
Senan Kelly <senan@senan.xyz>
2021-05-10 21:30:05 +0100

    use XDG_CONFIG_HOME for CORE_D_DOTFILE if available (#172)

--- bin/prettierd.js
+++ bin/prettierd.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
+const path = require("path");
+const fs = require("fs");
+
 process.env.CORE_D_TITLE = "prettierd";
 process.env.CORE_D_DOTFILE = ".prettierd";
 process.env.CORE_D_SERVICE = require.resolve("../dist/service");
+
+if (process.env.HOME && process.env.XDG_CONFIG_HOME) {
+  const confDir = path.join(
+    process.env.XDG_CONFIG_HOME,
+    process.env.CORE_D_TITLE
+  );
+  fs.mkdirSync(confDir, { recursive: true });
+
+  const confPath = path.join(confDir, process.env.CORE_D_TITLE);
+  process.env.CORE_D_DOTFILE = path.relative(process.env.HOME, confPath);
+}
 
 const { main } = require("../dist/main");
 
