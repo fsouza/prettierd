2e449fffc3242c7de0311c3768bf9bccf57eeac6
francisco souza <fsouza@users.noreply.github.com>
2021-08-30 19:41:46 -0400

    Implement support for plugins
    Users still need to specify it in the config file, but otherwise it
    should work!

--- src/service.ts
+++ src/service.ts
@@ -4,19 +4,82 @@ import LRU from "nanolru";
 import { dirname } from "path";
 import path from "path";
 import type Prettier from "prettier";
+import { promisify } from "util";
+import fs from "fs";
+
+const stat = promisify(fs.stat);
 
 const cacheParams = { max: 20, maxAge: 60000 };
 
-const configCache = new LRU(cacheParams);
+const caches = {
+  configCache: new LRU(cacheParams),
+  importCache: new LRU(cacheParams),
+  parentCache: new LRU(cacheParams),
+};
 
-const importCache = new LRU(cacheParams);
+async function isDir(path: string): Promise<boolean> {
+  try {
+    const fsStat = await stat(path);
+    return fsStat.isDirectory();
+  } catch (e) {
+    return false;
+  }
+}
+
+async function findParent(
+  start: string,
+  search: string
+): Promise<string | undefined> {
+  const cacheKey = `${start}|${search}`;
+  const cachedValue = caches.parentCache.get<string, string | false>(cacheKey);
+
+  if (cachedValue === false) {
+    return undefined;
+  }
+
+  if (cachedValue !== null) {
+    return cachedValue;
+  }
+
+  const parent = path.join(start, "..");
+  if (parent === start) {
+    caches.parentCache.set(cacheKey, false);
+    return undefined;
+  }
+
+  try {
+    const candidate = path.join(parent, search);
+    if (await isDir(candidate)) {
+      caches.parentCache.set(cacheKey, candidate);
+      return candidate;
+    }
+  } catch (e) {}
+
+  return await findParent(parent, search);
+}
+
+async function pluginSearchDirs(cwd: string): Promise<string[]> {
+  const result: string[] = [];
+
+  const localPath = path.join(cwd, "node_modules");
+  if (await isDir(localPath)) {
+    result.push(localPath);
+  }
+
+  const parentNodeModules = await findParent(__dirname, "node_modules");
+  if (parentNodeModules) {
+    result.push(parentNodeModules);
+  }
+
+  return result;
+}
 
 async function resolveConfig(
   prettier: typeof Prettier,
   cwd: string,
   filepath: string
 ): Promise<Prettier.Options> {
-  let v = configCache.get<string, Prettier.Options>(cwd);
+  let v = caches.configCache.get<string, Prettier.Options>(cwd);
 
   if (!v) {
     v = await prettier.resolveConfig(filepath, {
@@ -37,7 +100,7 @@ async function resolveConfig(
     }
 
     if (v) {
-      configCache.set(cwd, v);
+      caches.configCache.set(cwd, v);
     }
   }
 
@@ -48,7 +111,7 @@ async function resolveConfig(
 }
 
 async function resolvePrettier(cwd: string): Promise<typeof Prettier> {
-  const cached = importCache.get<string, typeof Prettier>(cwd);
+  const cached = caches.importCache.get<string, typeof Prettier>(cwd);
   if (cached) {
     return cached;
   }
@@ -56,7 +119,7 @@ async function resolvePrettier(cwd: string): Promise<typeof Prettier> {
   return import(require.resolve("prettier", { paths: [cwd] }))
     .catch(() => import("prettier"))
     .then((v) => {
-      importCache.set(cwd, v);
+      caches.importCache.set(cwd, v);
       return v;
     });
 }
@@ -75,7 +138,11 @@ async function run(cwd: string, args: string[], text: string): Promise<string> {
   const prettier = await resolvePrettier(cwd);
   const options = await resolveConfig(prettier, cacheKey, fullPath);
 
-  return prettier.format(text, options);
+  return prettier.format(text, {
+    ...options,
+    // @ts-ignore: pluginSearchDirs is not declared in the prettier interface :(
+    pluginSearchDirs: await pluginSearchDirs(cwd),
+  });
 }
 
 export function invoke(
