436f60a2044a4315e4215d79279e4ef82580758b
Joao Vitor <joaovitorsrs7@gmail.com>
2021-12-30 12:09:46 -0300

    refactor: new name for the iCommands interface now the new name is just Command.

--- src/args.ts
+++ src/args.ts
@@ -1,44 +1,44 @@
-interface iCommands {
+interface Command {
   command: string;
   description: string;
 }
 
-const commands: iCommands[] = [
+const commands: Command[] = [
   {
-    command: "--help",
-    description: "Show CLI usage.",
+    command: '--help',
+    description: 'Show CLI usage.',
   },
   {
-    command: "--version\t",
-    description: "Print prettierd version.",
+    command: '--version\t',
+    description: 'Print prettierd version.',
   },
   {
-    command: "--ignore-path <path>",
-    description: "Path to a file with patterns describing files to ignore.",
+    command: '--ignore-path <path>',
+    description: 'Path to a file with patterns describing files to ignore.',
   },
   {
-    command: "--no-color\t",
-    description: "Do not colorize error messages.",
+    command: '--no-color\t',
+    description: 'Do not colorize error messages.',
   },
   {
-    command: "start\t",
-    description: "Start the prettierd.",
+    command: 'start\t\t',
+    description: 'Start the prettierd.',
   },
   {
-    command: "stop\t",
-    description: "Stop the prettierd.",
+    command: 'stop\t\t',
+    description: 'Stop the prettierd.',
   },
   {
-    command: "restart\t",
-    description: "Restart the prettierd.",
+    command: 'restart\t',
+    description: 'Restart the prettierd.',
   },
   {
-    command: "status\t",
-    description: "Get the prettierd status.",
+    command: 'status\t\t',
+    description: 'Get the prettierd status.',
   },
   {
-    command: "invoke\t",
-    description: "Invoke the prettierd.",
+    command: 'invoke\t\t',
+    description: 'Invoke the prettierd.',
   },
 ];
 
