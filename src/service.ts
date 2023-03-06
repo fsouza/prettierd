fsouza/prettierd

commit cd6c0e00f5c1b90bf7a219702d50f9e3e47ee6f2

service: simplify code
Instead of splitting and joining stuff again, just pass a limit to
split.

src/service.ts
@@ -40,8 +40,8 @@ function argsToOptions(args: string[]) {
   };
 
   for (const arg of args) {
-    let [key, ...valueParts] = arg.replace(/^-+/, "").split("=");
-    let value: boolean | number | string = valueParts.join("=");
+    let [key, rawValue] = arg.replace(/^-+/, "").split("=", 2);
+    let value: boolean | number | string = rawValue;
     if (!value.length) {
       value = !key.startsWith("no-");
       if (!value) {

commit 2d5e947cd63e28170fe38daf8216d98c89ad6256

service: make sure we handle undefined rawValue
This is a regression introduced in #436.

I'll add some tests for this code later today.

src/service.ts
@@ -41,7 +41,7 @@ function argsToOptions(args: string[]) {
 
   for (const arg of args) {
     let [key, rawValue] = arg.replace(/^-+/, "").split("=", 2);
-    let value: boolean | number | string = rawValue;
+    let value: boolean | number | string = rawValue ?? "";
     if (!value.length) {
       value = !key.startsWith("no-");
       if (!value) {